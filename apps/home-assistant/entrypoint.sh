155baca61dbd434d762c4f64fa86e8a4f4a786d8
Devin Buhl <onedr0p@users.noreply.github.com>
2025-03-25 23:59:01 -0400

    Update entrypoint.sh

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -2,10 +2,12 @@
 #shellcheck disable=SC2086
 
 unset UV_SYSTEM_PYTHON
+
 mkdir -p "${VENV_FOLDER}"
 uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}"
 source "${VENV_FOLDER}/bin/activate"
-site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
+
+site_packages=$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")
 uv pip install --no-index --find-links="${site_packages}" uv
 
 ln -sf /proc/self/fd/1 /config/home-assistant.log
