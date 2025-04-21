7a44f4311cdbc1ae313ef96774b60951110399bc
Devin Buhl <devin@buhl.casa>
2025-04-21 08:42:25 -0400

    feat(transmission): allow disabling env substitution
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/transmission/entrypoint.sh
+++ apps/transmission/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2086
 
+# Only template the config file if our custom env variables are present
 if env | grep -q "^TRANSMISSION__"; then
     minijinja-cli --env /defaults/settings.json.j2 > /config/settings.json
 fi
@@ -9,5 +10,5 @@ exec \
     /usr/bin/transmission-daemon \
         --foreground \
         --config-dir /config \
-        --log-level "${TRANMISSIONS__LOG_LEVEL:-info}" \
+        --log-level "${TRANSMISSION_LOG_LEVEL:-info}" \
         "$@"
