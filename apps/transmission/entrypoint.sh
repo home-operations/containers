307b4e80655342b15e396c5dcbdc569f3f0bff14
Devin Buhl <devin@buhl.casa>
2025-04-21 08:27:38 -0400

    feat(transmission): allow disabling env substitution
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/transmission/entrypoint.sh
+++ apps/transmission/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2086
 
-minijinja-cli --env /defaults/settings.json.j2 > /config/settings.json
-
-if [[ "${TRANSMISSION__DEBUG}" == "true" ]]; then
-    echo "Transmission starting with the following configuration..."
-    cat /config/settings.json
+if [[ "${TRANSMISSION__USE_ENV}" == "true" ]]; then
+    minijinja-cli --env /defaults/settings.json.j2 > /config/settings.json
 fi
 
-#shellcheck disable=SC2086
 exec \
     /usr/bin/transmission-daemon \
         --foreground \
