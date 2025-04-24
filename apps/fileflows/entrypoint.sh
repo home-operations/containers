6320039ca81ad1baf0334fd7afe2f19105fa7983
ant385525 <ant385525@yahoo.com>
2025-04-24 00:39:31 -0700

    fix(fileflows) cd into correct dir before starting

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$FFNODE" = "0" ]; then
+    cd /app/Server
     echo "Starting Fileflows Server..."
     dotnet /app/Server/FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}"
 elif [ "$FFNODE" = "1" ]; then
+    cd /app/Node
     echo "Starting FileFlows Node..."
-    dotnet /app/Server/FileFlows.Node.dll
+    dotnet FileFlows.Node.dll
 else
     echo "Unknown FFNODE value: '$FFNODE'"
     echo "To run the node set the FFNODE env to '1'"
