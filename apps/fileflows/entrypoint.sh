0846177b0f5aa4359b445fe03fecaa538f81bcd6
ant385525 <ant385525@yahoo.com>
2025-04-23 20:58:05 -0700

    feat(fileflows) add entrypoint to start the app, ffmpeg from jellyfin image

---
+++ apps/fileflows/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$FFNODE" = "0" ]; then
+    echo "Starting Fileflows Server..."
+    dotnet /app/Server/FileFlows.Server.dll
+elif [ "$FFNODE" = "1" ]; then
+    echo "Starting FileFlows Node..."
+    dotnet /app/Server/FileFlows.Node.dll
+else
+    echo "Unknown FFNODE value: '$FFNODE'"
+    echo "To run the node set the FFNODE env to '1'"
+    exit 1
+fi
