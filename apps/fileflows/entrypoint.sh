f84e515926cd64770e0692c28cb8b78d920f1b1c
ant385525 <ant385525@yahoo.com>
2025-04-24 01:53:00 -0700

    fix(fileflows) add i18n hack to install plugins

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Complete i18n hack
+cp /var/tmp/i18n/* /app/Server/wwwroot/i18n
+
+# Start proper process
 if [ "$FFNODE" = "0" ]; then
     cd /app/Server
     echo "Starting Fileflows Server..."
