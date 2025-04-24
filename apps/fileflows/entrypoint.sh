f1493606e7aee9a2b5c7705a790df639f622028d
ant385525 <ant385525@yahoo.com>
2025-04-24 15:00:08 -0700

    fix(fileflows) add docker flag

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -2,13 +2,17 @@
 
 # Start proper process
 if [ "$FFNODE" = "0" ]; then
-    cd /app/Server
     echo "Starting Fileflows Server..."
-    dotnet /app/Server/FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}"
+    dotnet /app/Server/FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker true
 elif [ "$FFNODE" = "1" ]; then
+    if [ -z "$ServerUrl" ]; then
+        echo "Error: Running as a node but 'ServerUrl' environment variable is not set."
+        exit 1
+    fi
+
     cd /app/Node
     echo "Starting FileFlows Node..."
-    dotnet FileFlows.Node.dll
+    dotnet /app/Server/FileFlows.Node.dll --docker true
 else
     echo "Unknown FFNODE value: '$FFNODE'"
     echo "To run the node set the FFNODE env to '1'"
