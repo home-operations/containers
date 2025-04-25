1547272bb4414e913633170b7bc8009e53589a1f
ant385525 <ant385525@yahoo.com>
2025-04-24 19:02:22 -0700

    fix(fileflows) cd into correct dirs for wwwroot, etc paths

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -3,16 +3,17 @@
 # Start proper process
 if [ "$FFNODE" = "0" ]; then
     echo "Starting Fileflows Server..."
-    dotnet /app/Server/FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker
+    cd /app/Server
+    dotnet FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker
 elif [ "$FFNODE" = "1" ]; then
     if [ -z "$ServerUrl" ]; then
         echo "Error: Running as a node but 'ServerUrl' environment variable is not set."
         exit 1
     fi
 
-    cd /app/Node
     echo "Starting FileFlows Node..."
-    dotnet /app/Node/FileFlows.Node.dll --docker
+    cd /app/Node
+    dotnet FileFlows.Node.dll --docker
 else
     echo "Unknown FFNODE value: '$FFNODE'"
     echo "To run the node set the FFNODE env to '1'"
