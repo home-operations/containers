a1afb8db9a4d04b75494a3cb95ebe4790536e191
ant385525 <ant385525@yahoo.com>
2025-04-24 20:04:10 -0700

    fix(fileflows) try running dotnet with exec

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Start proper process
 if [ "$FFNODE" = "0" ]; then
     echo "Starting Fileflows Server..."
     cd /app/Server
-    dotnet FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker
+    exec dotnet FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker
 elif [ "$FFNODE" = "1" ]; then
     if [ -z "$ServerUrl" ]; then
         echo "Error: Running as a node but 'ServerUrl' environment variable is not set."
@@ -13,7 +14,7 @@ elif [ "$FFNODE" = "1" ]; then
 
     echo "Starting FileFlows Node..."
     cd /app/Node
-    dotnet FileFlows.Node.dll --docker true
+    exec dotnet FileFlows.Node.dll --docker true
 else
     echo "Unknown FFNODE value: '$FFNODE'"
     echo "To run the node set the FFNODE env to '1'"
