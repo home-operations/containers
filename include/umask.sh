537e980027418f561523fbdf37758a981ae4ecef
Devin Buhl <devin@buhl.casa>
2025-05-12 09:29:35 -0400

    feat: add umask functionality back
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- include/umask.sh
+++ include/umask.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Set default umask value if UMASK is not set or invalid
-UMASK="${UMASK:-0002}"
-if [[ ! "${UMASK}" =~ ^[0-7]{4}$ ]]; then
-    echo "Error: Invalid umask value. Using default value 0002."
-    UMASK="0002"
+if [[ -n "${UMASK}" ]]; then
+    if [[ "${UMASK}" =~ ^[0-7]{4}$ ]]; then
+        umask "${UMASK}"
+    else
+        echo "Error: Invalid umask value '${UMASK}'. It must be a four-digit octal number."
+    fi
 fi
-
-# Apply the umask
-umask "${UMASK}"
