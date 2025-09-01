020b10d473565ab311c00ec5ecf93f3baadd20ba
Devin Buhl <devin@buhl.casa>
2025-09-01 08:44:01 -0400

    ci: use mise in workflows
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/actions/app-tests/run.sh
+++ .github/actions/app-tests/run.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -euo pipefail
 
 APP="${1:?}"
 IMAGE="${2:?}"
 
-if [[ -x "$(command -v container-structure-test)" ]]; then
+if yq --exit-status '.schemaVersion' "./apps/${APP}/tests.yaml" &>/dev/null; then
     container-structure-test test --image "${IMAGE}" --config "./apps/${APP}/tests.yaml"
-elif [[ -x "$(command -v goss)" && -x "$(command -v dgoss)" ]]; then
+else
     export GOSS_FILE="./apps/${APP}/tests.yaml"
     export GOSS_OPTS="--retry-timeout 60s --sleep 1s"
     dgoss run "${IMAGE}"
-else
-    echo "No testing tool found. Exiting."
-    exit 1
 fi
