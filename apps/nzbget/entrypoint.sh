98f0cc141a8b9eb1b5c9919d2f6442e610581c4d
Devin Buhl <onedr0p@users.noreply.github.com>
2025-09-03 23:45:00 -0400

    fix(nzbget): Update configuration expressions in entrypoint.sh

--- apps/nzbget/entrypoint.sh
+++ apps/nzbget/entrypoint.sh
@@ -5,12 +5,22 @@ CONFIG_FILE="/config/nzbget.conf"
 if [[ ! -f "${CONFIG_FILE}" ]]; then
     cp /app/nzbget.conf "${CONFIG_FILE}"
     sed -i \
-        -e "s|^MainDir=.*|MainDir=/config/downloads|g" \
-        -e "s|^QueueDir=.*|QueueDir=/config/queue|g" \
-        -e "s|^LockFile=.*|LockFile=/config/nzbget.lock|g" \
-        -e "s|^LogFile=.*|LogFile=/config/nzbget.log|g" \
+        -e "s|^MainDir=.*|MainDir=/config|g" \
+        -e "s|^ScriptDir=.*|ScriptDir=$\{MainDir\}/scripts|g" \
+        -e "s|^WebDir=.*|WebDir=$\{AppDir\}/webui|g" \
+        -e "s|^ConfigTemplate=.*|ConfigTemplate=$\{AppDir\}/webui/nzbget.conf.template|g" \
+        -e "s|^UnrarCmd=.*|UnrarCmd=unrar|g" \
+        -e "s|^SevenZipCmd=.*|SevenZipCmd=7z|g" \
+        -e "s|^DestDir=.*|DestDir=$\{MainDir\}/completed|g" \
+        -e "s|^InterDir=.*|InterDir=$\{MainDir\}/intermediate|g" \
+        -e "s|^LogFile=.*|LogFile=$\{MainDir\}/nzbget.log|g" \
+        -e "s|^AuthorizedIP=.*|AuthorizedIP=127.0.0.1|g" \
         -e "s|^ShellOverride=.*|ShellOverride=.py=/usr/bin/python3;.sh=/bin/bash|g" \
         "${CONFIG_FILE}"
+fi
+
+if [[ -f /config/nzbget.lock ]]; then
+    rm /config/nzbget.lock
 fi
 
 exec \
