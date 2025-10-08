066ec82d4e8e1622dd11abd3e1f63fddba5c4cfe
Bernd Schorgers <me@bjw-s.dev>
2025-10-08 13:49:47 +0200

    feat: Introduce testhelpers

--- apps/deluge/container_test.go
+++ apps/deluge/container_test.go
@@ -2,30 +2,13 @@ package main
 
 import (
 	"context"
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
+	"github.com/home-operations/containers/testhelpers"
 )
 
 func Test(t *testing.T) {
 	ctx := context.Background()
-
-	image := os.Getenv("TEST_IMAGE")
-	if image == "" {
-		image = "ghcr.io/home-operations/deluge:rolling"
-	}
-
-	app, err := testcontainers.Run(
-		ctx, image,
-		testcontainers.WithExposedPorts("58846/tcp"),
-		testcontainers.WithWaitStrategy(
-			wait.ForListeningPort("58846/tcp"),
-		),
-	)
-	testcontainers.CleanupContainer(t, app)
-	require.NoError(t, err)
+	image := testhelpers.GetTestImage("ghcr.io/home-operations/deluge:rolling")
+	testhelpers.TestHTTPEndpoint(t, ctx, image, testhelpers.HTTPTestConfig{Port: "58846"}, nil)
 }
