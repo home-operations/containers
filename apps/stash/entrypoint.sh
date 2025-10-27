44f339040b166b53189aa687c283033f5cf2fab3
Devin Buhl <onedr0p@users.noreply.github.com>
2025-10-27 07:52:04 -0400

    Refactor entrypoint.sh for improved readability

--- apps/stash/entrypoint.sh
+++ apps/stash/entrypoint.sh
@@ -3,16 +3,16 @@
 export USER="stash"
 
 dirs=(
-  /config/plugins
-  /config/scrapers
+    /config/plugins
+    /config/scrapers
 )
 
 for dir in "${dirs[@]}"; do
-  find "$dir" -type f -name "requirements.txt" | while read -r reqfile; do
-    target_dir="$(dirname "$reqfile")"
-    echo "Installing Python requirements from: $reqfile"
-    pip install --target "$target_dir" --no-cache-dir --break-system-packages -r "$reqfile"
-  done
+    find "$dir" -type f -name "requirements.txt" | while read -r reqfile; do
+        target_dir="$(dirname "$reqfile")"
+        echo "Installing Python requirements from: $reqfile"
+        pip install --target "$target_dir" --no-cache-dir --break-system-packages -r "$reqfile"
+    done
 done
 
 exec /app/stash "@"
